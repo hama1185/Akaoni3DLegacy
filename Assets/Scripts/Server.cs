341b84af9246f62b2dd47aed8bd6d2f7f8b69381
hama1185 <hamataku7332@softbank.ne.jp>
2019-10-23 18:10:34 +0900

    Add OSCconnect

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityOSC;
+
+public class Server : MonoBehaviour{
+    // Start is called before the first frame update
+    #region Network Settings //----------追記
+	public string serverName;
+	public int inComingPort; //----------追記
+	#endregion //----------追記
+
+	private Dictionary<string, ServerLog> servers;
+    
+    void Start(){
+        OSCHandler.Instance.serverInit(serverName,inComingPort); //init OSC　//----------変更
+        servers = new Dictionary<string, ServerLog>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        OSCHandler.Instance.UpdateLogs();
+		servers = OSCHandler.Instance.Servers;
+
+		foreach( KeyValuePair<string, ServerLog> item in servers ){
+			// If we have received at least one packet,
+			// show the last received from the log in the Debug console
+			if(item.Value.log.Count > 0){
+				int lastPacketIndex = item.Value.packets.Count - 1;
+
+				// UnityEngine.Debug.Log(String.Format("SERVER: {0} ADDRESS: {1} VALUE 0: {2}",
+				// 	item.Key, // Server name
+				// 	item.Value.packets[lastPacketIndex].Address, // OSC address
+				// 	item.Value.packets[lastPacketIndex].Data[0].ToString())); //First data value
+				
+                if(item.Value.packets[lastPacketIndex].Address.ToString() == "/position"){
+                    Vector3 enemyPosition;
+                    enemyPosition.x = (float)item.Value.packets[lastPacketIndex].Data[0];
+                    enemyPosition.y = (float)item.Value.packets[lastPacketIndex].Data[1];
+                    enemyPosition.z = (float)item.Value.packets[lastPacketIndex].Data[0];
+                    EnemyPositionTracker.enemyPosition = enemyPosition;
+				}
+				
+			}
+		} 
+    }
+}
