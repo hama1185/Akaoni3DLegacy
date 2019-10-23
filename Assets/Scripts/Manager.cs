332bb44d700e310d551a6fec94b788ee431e9163
toneDrei79 <tone0709@icloud.com>
2019-10-23 17:01:06 +0900

    make basic rules

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
-    public static Vector3 characterSpawnPoint{set; get;}
+    static float time = 0.0f;
+    static bool preparedFlag = false;
+    static bool startFlag = false;
 
-    void Start() {
+    void Update() {
+        if (startFlag) {
+            time += Time.deltaTime;
+            if (time >= 100.0f) {
+                Debug.Log("Time Up");
+            }
+        }
     }
 
-    
+    public static void Prepared() {
+        preparedFlag = true;
+    }
+
+    public static void GameStart() {
+        startFlag = true;
+        GameObject.FindWithTag("Enemy").transform.GetChild(0).gameObject.SetActive(true);
+    }
 }
