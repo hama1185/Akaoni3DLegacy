a7f7968c2616a7ad45fd61fb996ca9317c823e86
toneDrei79 <tone0709@icloud.com>
2019-10-24 13:02:11 +0900

    add sounds

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour {
     static float MAX_SPEED = 10.0f;
 
+    // 緊張度パラメータ(仮)
+    public static float tension {set; get;} = 0.2f;
+
     public static float GetMAX_SPEED() {
         return MAX_SPEED;
     }
