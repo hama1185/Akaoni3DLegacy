02830ff97d767fe5bbde90659386ea5ae768b553
hama1185 <hamataku7332@softbank.ne.jp>
2019-10-24 10:14:54 +0900

    flatter enemy

--- Assets/Scripts/EnemyPositionTracker.cs
+++ Assets/Scripts/EnemyPositionTracker.cs
@@ -4,4 +4,7 @@ using UnityEngine;
 
 public class EnemyPositionTracker : MonoBehaviour {
     public static Vector3 enemyPosition{set; get;}
+    private void Update() {
+        this.transform.position = enemyPosition;
+    }
 }
