8ed2ff6e0ee7b60dc7bf0446d9905fe9f9d8050b
toneDrei79 <tone0709@icloud.com>
2019-10-25 17:30:44 +0900

    make red view for ogre

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour {
+    static float density {set; get;} = 0.01f;
+    float time = 0.0f;
+    float stroke;
+
+    void Start() {
+        RenderSettings.fogMode = FogMode.Exponential;
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = Color.red;
+        RenderSettings.fogDensity = density;
+
+        stroke = 1.0f / (PlayerStatus.tension);
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            PlayerStatus.tension += 0.1f * Time.deltaTime;
+            Debug.Log("Player : " + PlayerStatus.tension);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            PlayerStatus.tension -= 0.1f * Time.deltaTime;
+            Debug.Log("Player : " + PlayerStatus.tension);
+        }
+
+        density = 0.01f + PlayerStatus.tension / 25.0f;
+        RenderSettings.fogDensity = density;
+
+        time += Time.deltaTime;
+    }
+}
