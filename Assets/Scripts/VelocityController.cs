a43104172f144925e2d3ffc41d961e72589b315b
toneDrei79 <tone0709@icloud.com>
2019-11-11 13:58:22 +0900

    resetting fixed timestep and change client.cs FixedUpdate -> Update (keep 5 FPS)

--- Assets/Scripts/VelocityController.cs
+++ Assets/Scripts/VelocityController.cs
@@ -8,23 +8,26 @@ public class VelocityController : MonoBehaviour {
     float MAX_SPEED = 5.0f;
     float speed;
     (string moveH, string moveV, string viewH, string viewV) key;
-    
+
     void Start() {
-        speed = 5.0f;
+        speed = 10.0f;
         key = KeyConfig.SetKeyConfig();
     }
 
     void Update() {
-		// ゲームパッドによる速度入力
-        float moveLR = 0.0f;
-        float moveFB = 0.0f;
-        moveLR = Input.GetAxis(key.moveH) * speed;
-	    moveFB = Input.GetAxis(key.moveV) * speed;
+		// // // ゲームパッドによる速度入力
 
-        inputAxis_Left = new Vector3(moveLR, 0.0f, moveFB);
-        inputAxis_Left = this.transform.rotation * inputAxis_Left;
+        // float moveLR = 0.0f;
+        // float moveFB = 0.0f;
+        // moveLR = Input.GetAxis(key.moveH) * speed;
+	    // moveFB = Input.GetAxis(key.moveV) * speed;
 
-        //
+        // inputAxis_Left = new Vector3(moveLR, 0.0f, moveFB);
+        // inputAxis_Left = this.transform.rotation * inputAxis_Left;
+
+        // // //
+
+        inputAxis_Left *= speed;
 
         float magnitude = Mathf.Sqrt(Mathf.Pow(inputAxis_Left.x, 2) + Mathf.Pow(inputAxis_Left.z, 2));
         if (magnitude > MAX_SPEED) {
