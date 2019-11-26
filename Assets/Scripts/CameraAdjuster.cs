722bc7280182cd5a7d4bb36b41bf682f39adf531
hama1185 <hamataku7332@softbank.ne.jp>
2019-11-26 18:18:49 +0900

    add Scene

---
+++ Assets/Scripts/CameraAdjuster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour
+{
+    public static float sentAngle{set;get;}
+    GameObject PhoneCam;
+    // Start is called before the first frame update
+    void Start(){
+        PhoneCam = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float subtraction = sentAngle - PhoneCam.transform.rotation.eulerAngles.y;
+        //まだ修正
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0.0f, -subtraction, 0.0f));
+    }
+}
---
+++ Assets/Scripts/OperationKey.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OperationKey : MonoBehaviour
+{
+    public static Vector3 mousePosition;
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+        mousePosition = Input.mousePosition;
+        Vector3 transPositon = this.transform.position;
+        transPositon.x = ((mousePosition.x - 1280) + 0.01f) / 8;
+        transPositon.y = ((mousePosition.y - 720) + 0.01f) / 8;
+        this.transform.position = transPositon;
+        
+
+        if(Input.GetMouseButtonUp(0)){
+            if((this.transform.position.x >= 13.7 && this.transform.position.x <= 20.4) && 
+            (this.transform.position.y >= -4.5 && this.transform.position.y <= 3.7)){
+                SceneManager.LoadScene("VillagersScene");
+            }
+            if((this.transform.position.x >= -19.6 && this.transform.position.x <= -12.9) && 
+            (this.transform.position.y >= -4.5 && this.transform.position.y <= 3.7)){
+                SceneManager.LoadScene("OgreScene");
+            }        
+        }
+    }
+}
